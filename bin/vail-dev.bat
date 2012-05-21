d081654c21eccca5ec3bbaab24c8ab00e94936d4
Chanwit Kaewkasi <chanwit@gmail.com>
2012-05-21 21:26:55 +0700

    compile vail-web-server busmod every time

--- bin/vail-dev.bat
+++ bin/vail-dev.bat
@@ -3,4 +3,7 @@
 cd vail-bootstrap
 call ../gradlew clean assemble install
 cd ..
+
+javac -cp vertx\lib\jars\vert.x-core.jar;vertx\lib\jars\vert.x-platform.jar vertx\mods\vail-web-server\org\vertx\mods\VailWebServer.java
+
 call bin/vail.bat
