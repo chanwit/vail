65620368f2687313f3ebd85a0d55d082ca885925
Chanwit Kaewkasi <chanwit@gmail.com>
2012-05-22 22:01:38 +0700

    change main class

--- bin/vail.bat
+++ bin/vail.bat
@@ -2,4 +2,4 @@
 
 for %%? in ("%~dp0..") do set VAIL_HOME=%%~f?
 set VERTX_HOME=%VAIL_HOME%\vertx
-"%JAVA_HOME%\bin\java" -cp dist\vail-bootstrap.jar;%VERTX_HOME%\lib\jars\js.jar org.vail.bootstrap.cli.Starter %*
+"%JAVA_HOME%\bin\java" -cp dist\vail-bootstrap.jar;%VERTX_HOME%\lib\jars\js.jar org.vailframework.bootstrap.cli.Starter %*
